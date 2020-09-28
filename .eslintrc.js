1500567fff3a432e09a567bdda9a5603be06f6d4
Marvin Parada <hello@marvin.dev>
2020-09-28 12:34:35 -0700

    NEW: Install Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/prettier'],
+  plugins: ['vue', 'prettier'],
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: 'babel-eslint',
   },
   rules: {
+    'prettier/prettier': ['error'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         mocha: true,
       },
