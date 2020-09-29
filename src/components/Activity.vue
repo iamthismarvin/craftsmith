0a1e11a3ad9607f5e082c914dbbef7a4148a0af0
Marvin Parada <hello@marvin.dev>
2020-09-28 22:54:16 -0700

    Create Activity component.

---
+++ src/components/Activity.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="activity text-white">
+    <div class="bg-gray-800 p-2 text-center">Activity</div>
+    <div ref="activity" class="bg-gray-900 overflow-auto h-64 px-1">
+      <div v-for="entry in entries" :key="entry.id">
+        {{ entry }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'activity',
+  props: {
+    entries: Array,
+  },
+  mounted() {
+    this.scrollToBottom();
+  },
+  updated() {
+    this.scrollToBottom();
+  },
+  methods: {
+    scrollToBottom() {
+      const { activity } = this.$refs;
+      activity.scrollTop = activity.scrollHeight;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,57 @@
 <template>
-  <div>
-    <div class="home">This is the Home view.</div>
+  <div class="home">
+    <Console :entries="log" />
+    <div class="flex flex-col just justify-center items-center">
+      <h1 class="font-bold">Inventory</h1>
+      <p>Materials: {{ inventory.materials }}</p>
+      <p>Items: {{ inventory.items }}</p>
+    </div>
+    <button class="button bg-green-600 hover:bg-green-700" @click="gatherMaterials">
+      Gather Materials
+    </button>
+    <button class="button bg-purple-600 hover:bg-purple-700" @click="craftItem">
+      Craft Item
+    </button>
   </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
 export default {
   name: 'Home',
+  data: () => ({
+    log: [],
+    inventory: {
+      items: 0,
+      materials: 0,
+    },
+  }),
+  components: {
+    Console: defineAsyncComponent(() => import('@/components/Activity.vue')),
+  },
+  methods: {
+    gatherMaterials() {
+      const materials = Math.floor(Math.random() * 25);
+      this.inventory.materials += materials;
+      this.log.push(`You gathered ${materials} materials!`);
+    },
+    craftItem() {
+      const requiredMaterials = 25;
+      if (this.inventory.materials >= requiredMaterials) {
+        this.inventory.materials -= requiredMaterials;
+        this.inventory.items += 1;
+        this.log.push('You crafted an item!');
+      } else {
+        this.log.push('Insufficient materials to craft item.');
+      }
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.button {
+  @apply text-white w-full p-4;
+}
+</style>
