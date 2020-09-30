4387566ab2953743911e877cca2fba5c6c2a720e
Marvin Parada <hello@marvin.dev>
2020-09-30 15:49:51 -0700

    NEW: Add Home to Menu.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex justify-around bg-gray-900 h-16 w-screen text-white">
     <MenuItem
+      name="Home"
+      route="Home"
+      icon="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+    />
+    <MenuItem
       name="Craft"
       route="Craft"
       icon="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
