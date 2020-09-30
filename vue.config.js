0a603a5aeea55392a155e88eea4eaceb29eb425c
Marvin Parada <hello@marvin.dev>
2020-09-29 21:29:08 -0700

    NEW: Minor PWA configuration.

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,16 @@ module.exports = {
       },
     },
   },
+  pwa: {
+    name: 'Craftsmith',
+    themeColor: '#44337a',
+    backgroundColor: '#000000',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
+    },
+  },
 };
