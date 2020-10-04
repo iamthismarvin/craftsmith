384a53d76b16d035f5a9dbf581bfae386658041b
Marvin Parada <hello@marvin.dev>
2020-10-04 00:05:46 -0700

    NEW: Create ItemPreview component.

---
+++ src/components/ItemPreview.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="relative flex justify-center h-20 items-center w-20">
+    <img
+      :src="require(`@/assets/items/${image}`)"
+      alt="item"
+      class="item-preview__image absolute bg-gray-900 border-solid rounded-md"
+      :style="{ 'border-color': rarityColor }"
+    />
+    <div
+      v-if="enchantment > 0"
+      class="item-preview__enchantment absolute bg-purple-600 flex justify-center h-6 items-center rounded-full w-6"
+      :style="{ 'background-color': rarityColor }"
+    >
+      {{ enchantment }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ItemPreview',
+  props: {
+    enchantment: Number,
+    image: String,
+    rarity: Number,
+  },
+  computed: {
+    rarityColor() {
+      let color;
+      switch (this.rarity) {
+        case 0: {
+          color = '#4a5568';
+          break;
+        }
+        case 1: {
+          color = '#38a169';
+          break;
+        }
+        case 2: {
+          color = '#4299e1';
+          break;
+        }
+        case 3: {
+          color = '#805ad5';
+          break;
+        }
+        case 4: {
+          color = '#dd6b20';
+          break;
+        }
+        case 5: {
+          color = 'red';
+          break;
+        }
+        default:
+          color = '#4a5568';
+          break;
+      }
+      return color;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.item-preview {
+  &__enchantment {
+    transform: translate(-1.8rem, -1.8rem);
+  }
+  &__image {
+    border-width: 0.25rem;
+  }
+}
+</style>
--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="p-2">
     <!-- TODO: Create weapon image component. Include enchantment and rarity. -->
+    <ItemPreview
+      :enchantment="item.enchantment"
+      :image="item.image"
+      :rarity="item.rarity"
+      class="flex justify-center items-center w-full"
+    />
     <button @click="enchantItem" class="bg-purple-600 hover:bg-purple-700 mb-1">Enchant</button>
     <button @click="dismantleItem" class="bg-orange-600 hover:bg-orange-700 mb-1">Dismantle</button>
     <button @click="sellItem" class="bg-green-600 hover:bg-green-700 mb-1">Sell</button>
@@ -8,15 +14,29 @@
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue';
+
 export default {
   name: 'Item',
+  data: () => ({
+    item: {
+      enchantment: 0,
+      image: 'iron_sword.png',
+      rarity: 0,
+    },
+  }),
+  components: {
+    ItemPreview: defineAsyncComponent(() => import('@/components/ItemPreview.vue')),
+  },
   methods: {
     // Make the following methods mutations and actions.
     enchantItem() {
       console.log('Enchanting Item. There is a possibility it may fail and lower enchantment.');
+      this.item.enchantment += 1;
     },
     dismantleItem() {
       console.log('Dismantling Item. You will gain item knowledge and get back some materials.');
+      this.item.rarity += 1;
     },
     sellItem() {
       console.log('Selling Item. Money pleeease!');
