4b843e86c6b6d8610f08ba546be61faece38beeb
Marvin Parada <hello@marvin.dev>
2021-01-11 14:07:44 -0800

    NEW: Create weapons.js asset data.

---
+++ src/assets/data/weapons.js
@@ -0,0 +1,22 @@
+const weapons = [
+  {
+    id: 0,
+    name: 'Sword of Beginnings',
+    type: 0,
+    attack: {
+      min: 100,
+      max: 200,
+    },
+  },
+  {
+    id: 1,
+    name: 'Greatsword of Beginnings',
+    type: 1,
+    attack: {
+      min: 150,
+      max: 250,
+    },
+  },
+];
+
+export default weapons;
