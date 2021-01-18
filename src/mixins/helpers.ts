a59fdea809453887382b3db136bd4f14d0591223
Marvin Parada <hello@marvin.dev>
2021-01-18 13:02:13 -0800

    NEW: Change [helpers] mixin to Typescript.

--- src/mixins/helpers.ts
+++ src/mixins/helpers.ts
@@ -1,21 +1,25 @@
 import weapons from '@/assets/data/weapons';
+import { Item } from '@/utilities/interfaces';
 
 const helpers = {
   methods: {
-    getRatingColor(rating) {
+    getRatingColor(rating: number) {
       const colors = ['#ffffff', '#4299e1', '#805ad5', '#dd6b20', '#ffff00', '#ff0000'];
       return colors[rating];
     },
-    getRatingName(rating) {
+    getRatingName(rating: number) {
       const names = ['Common', 'Rare', 'Epic', 'Unique', 'Legendary', 'Myth'];
       return names[rating];
     },
-    getWeaponType(type) {
+    getWeaponType(type: number) {
       const types = ['Sword', 'Greatsword', 'Staff', 'Bow'];
       return types[type];
     },
-    getItemData(item) {
+    getItemData(item: Item) {
       const assetItem = weapons.find(weapon => weapon.id === item.baseItem);
+      if (!assetItem) {
+        throw console.log('Item data not found.');
+      }
       const itemData = {
         id: item.id,
         enchantment: item.enchantment,
