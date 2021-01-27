40af516f4c16856efd1ae7226c8bebd69e82f56b
Marvin Parada <hello@marvin.dev>
2021-01-27 15:33:27 -0800

    NEW: Redesign & Replace [getTurnOrder] combat function.

--- src/views/Combat.vue
+++ src/views/Combat.vue
@@ -6,8 +6,10 @@
     <h3>Enemy:</h3>
     <Bar type="health" :percentage="enemy.health" />
     <h3>Location: {{ location }}</h3>
-    <button @click="basicAttack('player')">Attack Player</button>
-    <button @click="basicAttack('enemy')">Attack Enemy</button>
+    <button v-if="currentCombatant === 'ENEMY'" @click="basicAttack('player')">
+      Attack Player
+    </button>
+    <button v-if="currentCombatant === 'PLAYER'" @click="basicAttack('enemy')">Attack Enemy</button>
     <button @click="turn += 1">Add Turn</button>
   </div>
 </template>
@@ -20,6 +22,14 @@ export default {
   name: 'Combat',
   data: () => ({
     turn: 0,
+    speedCounters: {
+      enemy: 0,
+      player: 0,
+    },
+    attackSpeed: {
+      enemy: 1,
+      player: 1.25,
+    },
   }),
   components: {
     Bar: defineAsyncComponent(() => import('@/components/Combat/Bar.vue')),
@@ -31,7 +41,18 @@ export default {
       enemy: 'combat/enemy',
     }),
     currentCombatant() {
-      return this.getTurnOrder(this.turn, 1.25, 1)[this.turn] === 'p' ? 'Player' : 'Enemy';
+      return this.getCurrentCombatant(this.speedCounters.player, this.speedCounters.enemy);
+    },
+    nextCombatants() {
+      const speedCounters = { ...this.speedCounters };
+      const turnOrder = [];
+      for (let i = 0; i <= 5; i += 1) {
+        const combatant = this.getCurrentCombatant(speedCounters.player, speedCounters.enemy);
+        this.updateSpeedCounters(combatant, speedCounters);
+        turnOrder.push(combatant);
+      }
+      turnOrder.shift();
+      return turnOrder;
     },
   },
   methods: {
@@ -40,35 +61,22 @@ export default {
       SET_COMBAT_STATE: 'combat/SET_COMBAT_STATE',
       MODIFY_TARGET_HEALTH: 'combat/MODIFY_TARGET_HEALTH',
     }),
+    updateSpeedCounters(combatant, speedCountersPointer) {
+      const speedCounters = speedCountersPointer;
+      if (combatant === 'PLAYER') {
+        speedCounters.player -= 1;
+        speedCounters.enemy += this.attackSpeed.enemy;
+      } else {
+        speedCounters.enemy -= 1;
+        speedCounters.player += this.attackSpeed.player;
+      }
+    },
     basicAttack(target) {
       this.MODIFY_TARGET_HEALTH([target, -10]);
+      this.turn += 1;
     },
-    getTurnOrder(currentTurn, playerAttackSpeed, enemyAttackSpeed) {
-      const turnOrder = [];
-      let counter = 0;
-      let playerCounter = 0;
-      let enemyCounter = 0;
-      while (counter < currentTurn + 1) {
-        playerCounter += playerAttackSpeed;
-        enemyCounter += enemyAttackSpeed;
-        while (playerCounter >= 1 || enemyCounter >= 1) {
-          if (playerCounter >= enemyCounter) {
-            turnOrder.push('p');
-            playerCounter -= 1;
-          }
-          if (enemyCounter > playerCounter) {
-            turnOrder.push('e');
-            enemyCounter -= 1;
-          }
-        }
-        counter += 1;
-      }
-      return turnOrder;
-    },
-    getTurnInfo() {
-      this.CREATE_LOG_ENTRY(`Current Turn #: ${this.turn}`);
-      this.CREATE_LOG_ENTRY(`Turn Order: ${this.getTurnOrder(this.turn, 1.25, 1)}`);
-      this.CREATE_LOG_ENTRY(`Current Combatant: ${this.currentCombatant}`);
+    getCurrentCombatant(playerSpeedCounter, enemySpeedCounter) {
+      return playerSpeedCounter >= enemySpeedCounter ? 'PLAYER' : 'ENEMY';
     },
   },
   mounted() {
@@ -77,11 +85,12 @@ export default {
       enemy: { health: 100, ready: true, weapon: 0 },
       location: 100,
     });
-    this.getTurnInfo();
+    this.CREATE_LOG_ENTRY(`${this.currentCombatant}'s turn.`);
   },
   watch: {
     turn() {
-      this.getTurnInfo();
+      this.updateSpeedCounters(this.currentCombatant, this.speedCounters);
+      this.CREATE_LOG_ENTRY(`${this.currentCombatant}'s turn.`);
     },
   },
 };
