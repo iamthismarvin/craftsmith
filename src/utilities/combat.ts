77dc8ff25c703ad7c4e41ebf7f42e672bfdfa6f8
Marvin Parada <hello@marvin.dev>
2021-03-03 15:37:42 -0800

    NEW: Create [getDungeonData] combat utility.

--- src/utilities/combat.ts
+++ src/utilities/combat.ts
@@ -1,5 +1,10 @@
+import dungeons from '@/assets/data/dungeons';
 import monsters from '@/assets/data/monsters';
 import weapons from '@/assets/data/weapons';
+
+export const getDungeonData = (id: number) => {
+  return dungeons.find(dungeon => dungeon.id === id);
+};
 
 export const getMonsterData = (id: number) => {
   return monsters.find(monster => monster.id === id);
